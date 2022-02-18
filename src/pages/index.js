06b2132ecef9d85ecbfbc11d39edbc46352136cc
thatanjan <anjan.cules@gmail.com>
2022-02-18 19:34:21 +0600

    unnecessary code is removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,10 @@
 import Typography from '@mui/material/Typography'
 
-import Login from '@/components/Login'
-
-// import Login from '@/a/b'
-
 const Home = () => {
 	return (
-		<div>
+		<>
 			<Typography variant='h1'>Home</Typography>
-
-			<Login />
-		</div>
+		</>
 	)
 }
 
